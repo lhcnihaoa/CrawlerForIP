2e1cb773153aaee002a9bed9901efdb33a665905
linhc <lhcnihaoa@qq.com>
2017-04-12 10:16:55 +0800

    增加速度和连接时间筛选，增加telnet测试

--- CrawlerForIP.py
+++ CrawlerForIP.py
@@ -1,9 +1,10 @@
-from lxml import  etree
+import telnetlib
 from bs4 import BeautifulSoup
 from urllib import error
 import requests
 import csv
 import os
+import time
 
 import Settings
 
@@ -22,29 +23,38 @@ def gethtml(url):
 
 class ProxyIP():
 
-    def __init__(self, ip, port, type):
+    def __init__(self, ip, port, type, speed, connect_time):
         self.ip = ip
         self. port = port
         self.type = type
+        self.speed = speed
+        self.connect_time = connect_time
 
 
-
-for i in range(1,5):
-    url = "http://www.xicidaili.com/nn/1"
+ip_record = []
+for i in range(1, 5):
+    url = "http://www.xicidaili.com/nn/" + str(i)
     page = gethtml(url)
 
     ip_list = page.find_all("table", {"id": "ip_list"})[0].find_all("tr")
-    ip_record = []
     for record in ip_list:
         if record.find_all("td"):
             country = record.find_all("td")[0].get_text()
             ip = record.find_all("td")[1].get_text()
             port = record.find_all("td")[2].get_text()
             type = record.find_all("td")[5].get_text()
-
-            proxies = {type: ip+port}
-            if requests.get("https://www.baidu.com", proxies=proxies).status_code==200:
-                ip_record.append(ProxyIP(ip, port, type))
+            speed = record.find_all("td")[6].find_all("div", {"class": "bar"})[0].attrs["title"].strip("秒")
+            connect_time = record.find_all("td")[7].find_all("div", {"class": "bar"})[0].attrs["title"].strip("秒")
+            if speed<0.5 and connect_time<0.5:
+                time.sleep(0.2)
+                try:
+                    telnetlib.Telnet(ip, port=port, timeout=0.5)
+                except:
+                    pass
+                else:
+                    proxies = {type: ip+port}
+                    if requests.get("https://www.baidu.com", proxies=proxies).status_code == 200:
+                        ip_record.append(ProxyIP(ip, port, type, speed, connect_time))
 with open("D:/MyPhyonProjects/ip_record.csv", 'a+', newline='') as csvFile:
     writer = csv.writer(csvFile)
     if os.path.getsize('D:/MyPhyonProjects/ip_record.csv') == 0:
