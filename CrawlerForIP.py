35b860888ea790ddf3dd4c74c522018ec5b72796
linhc <lhcnihaoa@qq.com>
2017-04-12 15:49:42 +0800

    通过IP138网页验证IP地址是否可用

--- CrawlerForIP.py
+++ CrawlerForIP.py
@@ -4,6 +4,7 @@ from urllib import error
 import requests
 import csv
 import os
+import re
 import time
 
 import Settings
@@ -32,7 +33,7 @@ class ProxyIP():
 
 
 ip_record = []
-for i in range(1, 5):
+for i in range(1, 10):
     url = "http://www.xicidaili.com/nn/" + str(i)
     page = gethtml(url)
 
@@ -42,22 +43,26 @@ for i in range(1, 5):
             country = record.find_all("td")[0].get_text()
             ip = record.find_all("td")[1].get_text()
             port = record.find_all("td")[2].get_text()
-            type = record.find_all("td")[5].get_text()
+            type = record.find_all("td")[5].get_text().lower()
             speed = record.find_all("td")[6].find_all("div", {"class": "bar"})[0].attrs["title"].strip("秒")
             connect_time = record.find_all("td")[7].find_all("div", {"class": "bar"})[0].attrs["title"].strip("秒")
-            if speed<0.5 and connect_time<0.5:
-                time.sleep(0.2)
+            if float(speed) < 2and float(connect_time) < 2:
+                time.sleep(0.1)
+                proxies = {'http': 'http'+"://"+ip+":"+port}
                 try:
-                    telnetlib.Telnet(ip, port=port, timeout=0.5)
+                    page = requests.get("http://1212.ip138.com/ic.asp", proxies=proxies, timeout=2)
                 except:
-                    pass
+                    print("failed to connect")
                 else:
-                    proxies = {type: ip+port}
-                    if requests.get("https://www.baidu.com", proxies=proxies).status_code == 200:
-                        ip_record.append(ProxyIP(ip, port, type, speed, connect_time))
-with open("D:/MyPhyonProjects/ip_record.csv", 'a+', newline='') as csvFile:
-    writer = csv.writer(csvFile)
-    if os.path.getsize('D:/MyPhyonProjects/ip_record.csv') == 0:
-        writer.writerow(['ip', 'port', 'type'])
-    for record in ip_record:
-        writer.writerow([record.ip, record.port, record.type])
+                    if page.status_code == 200:
+                        ip_ban = ['222.44.86.190', '222.44.86.185']
+                        ip_exp = re.compile('\d+\.\d+\.\d+\.\d+')
+                        ip_content = ip_exp.findall(BeautifulSoup(page.content, "lxml").get_text())[0]
+                        if ip_content not in ip_ban:
+                            # ip_record.append(ProxyIP(ip, port, type, speed, connect_time))
+                            with open("D:/MyPythonProjects/ip_record.csv", 'a+', newline='') as csvFile:
+                                writer = csv.writer(csvFile)
+                                if os.path.getsize('D:/MyPythonProjects/ip_record.csv') == 0:
+                                    writer.writerow(['ip', 'port', 'type'])
+                                else:
+                                    writer.writerow([ip, port, type])
